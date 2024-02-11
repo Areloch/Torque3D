4bcb999768ae6f07e43254ffb7addfb6a4c9d7f7
Areloch <Areloch@gmail.com>
2024-02-11 10:34:14 -0600

    Added graph logic to metrics entries

--- Engine/source/util/metricsTracker.cpp
+++ Engine/source/util/metricsTracker.cpp
@@ -6,32 +6,38 @@ MetricsTracker gMetricsTracker;
 
 MetricsTracker::MetricsTracker() = default;
 
-void MetricsTracker::addMetric(StringTableEntry metricsGroup, const String& metricsVariable, const String& metricsDescription)
+void MetricsTracker::addMetric(StringTableEntry metricsGroup, const String& metricsVariable, const String& metricsDescription, const bool& graph, const LinearColorF& graphColor, const S32& graphIntervalRate)
 {
    for (MetricsEntry& i : mMetricsList)
    {
       if (i.metricsGroup == metricsGroup)
       {
-         for (const MetricsEntry::StringPair& j : i.metricsVarEntry)
+         for (const MetricsVariable& j : i.metricsVarEntry)
          {
             if (j.metricsVariable == metricsVariable)
                return;
          }
 
-         MetricsEntry::StringPair newPair;
-         newPair.metricsVariable = metricsVariable;
-         newPair.metricsDescription = metricsDescription;
-         i.metricsVarEntry.push_back(newPair);
+         MetricsVariable newVar;
+         newVar.metricsVariable = metricsVariable;
+         newVar.metricsDescription = metricsDescription;
+         newVar.graph = graph;
+         newVar.graphColor = graphColor;
+         newVar.graphIntervalRate = graphIntervalRate;
+         i.metricsVarEntry.push_back(newVar);
          return;
       }
    }
 
    MetricsEntry newEntry;
    newEntry.metricsGroup = metricsGroup;
-   MetricsEntry::StringPair newPair;
-   newPair.metricsVariable = metricsVariable;
-   newPair.metricsDescription = metricsDescription;
-   newEntry.metricsVarEntry.push_back(newPair);
+   MetricsVariable newVar;
+   newVar.metricsVariable = metricsVariable;
+   newVar.metricsDescription = metricsDescription;
+   newVar.graph = graph;
+   newVar.graphColor = graphColor;
+   newVar.graphIntervalRate = graphIntervalRate;
+   newEntry.metricsVarEntry.push_back(newVar);
    mMetricsList.push_back(newEntry);
 }
 
@@ -41,7 +47,7 @@ void MetricsTracker::removeMetric(StringTableEntry metricsGroup, const String& m
    {
       if (i.metricsGroup == metricsGroup)
       {
-         for (Vector<MetricsEntry::StringPair>::iterator it = i.metricsVarEntry.begin(); it != i.metricsVarEntry.end(); it++)
+         for (Vector<MetricsVariable>::iterator it = i.metricsVarEntry.begin(); it != i.metricsVarEntry.end(); it++)
          {
             if (it->metricsVariable == metricsVariable)
             {
@@ -94,13 +100,36 @@ const char* MetricsTracker::getMetricsDescription(StringTableEntry metricsGroup,
    return "";
 }
 
-DefineEngineFunction(addMetric, void, (const char* metricsGroup, const char* metricsVariable, const char* metricDescription), ("", "", ""), "()"
+const char* MetricsTracker::getMetricsGraphInfo(StringTableEntry metricsGroup, const S32& index)
+{
+   for (const MetricsEntry& i : mMetricsList)
+   {
+      if (i.metricsGroup == metricsGroup)
+      {
+         if (index < i.metricsVarEntry.size() && index >= 0)
+         {
+            static constexpr U32 bufSize = 64;
+            char* retBuffer = Con::getReturnBuffer(bufSize);
+            dSprintf(retBuffer, bufSize, "%d\t%g %g %g\t%d", i.metricsVarEntry[index].graph,
+               i.metricsVarEntry[index].graphColor.red, i.metricsVarEntry[index].graphColor.green, i.metricsVarEntry[index].graphColor.blue,
+               i.metricsVarEntry[index].graphIntervalRate);
+            return retBuffer;
+         }
+      }
+   }
+   return "";
+}
+
+DefineEngineFunction(addMetric, void, (const char* metricsGroup, const char* metricsVariable, const char* metricDescription, bool graph, LinearColorF graphColor, S32 graphIntervalRate), ("", "", "", false, LinearColorF::WHITE, 0), "()"
    "@brief Adds a new metric to the tracker so it can be easily displayed via a UI or the console\n"
    "@param metricsGroup - The group to which the metric belongs. This is used to organize the metrics in the UI\n"
    "@param metricsVariable - The variable to be tracked as a metric\n"
+   "@param graph - Is this variable meant to be visually graphed in the profiler\n"
+   "@param graphColor - If graphed, what color is used\n"
+   "@param graphIntervalRate - If graphed, what is the polling rate on the graph?\n"
    "@ingroup Utils")
 {
-   gMetricsTracker.addMetric(StringTable->insert(metricsGroup), metricsVariable, metricDescription);
+   gMetricsTracker.addMetric(StringTable->insert(metricsGroup), metricsVariable, metricDescription, graph, graphColor, graphIntervalRate);
 }
 
 DefineEngineFunction(removeMetric, void, (const char* metricsGroup, const char* metricsVariable), ("", ""), "()"
@@ -138,3 +167,13 @@ DefineEngineFunction(getMetricsDescription, const char*, (const char* metricsGro
 {
    return gMetricsTracker.getMetricsDescription(StringTable->insert(metricsGroup), index);
 }
+
+DefineEngineFunction(getMetricsGraphInfo, const char*, (const char* metricsGroup, S32 index), ("", -1), "()"
+   "@brief Returns the graph info of the metric at the specified index\n"
+   "@param metricsGroup - The group to which the metrics belong\n"
+   "@param index - The index of the metric\n"
+   "@ingroup Utils")
+{
+   return gMetricsTracker.getMetricsGraphInfo(StringTable->insert(metricsGroup), index);
+}
+
--- Engine/source/util/metricsTracker.h
+++ Engine/source/util/metricsTracker.h
@@ -6,22 +6,26 @@
 #include "platform/platform.h"
 #include "console/console.h"
 #include "console/engineAPI.h"
+#include "core/color.h"
 
 struct MetricsTracker
 {
 private:
+   struct MetricsVariable
+   {
+      String metricsVariable;
+      String metricsDescription;
+      bool graph;
+      LinearColorF graphColor;
+      S32 graphIntervalRate;
+   };
+
    struct MetricsEntry
    {
       StringTableEntry metricsGroup{};
 
       //We use this instead of a Map to ensure that the order of registration is preserved
-      struct StringPair
-      {
-         String metricsVariable;
-         String metricsDescription;
-      };
-
-      Vector<StringPair> metricsVarEntry;
+      Vector<MetricsVariable> metricsVarEntry;
    };
 
    Vector<MetricsEntry> mMetricsList;
@@ -34,8 +38,12 @@ public:
    /// <param> metricsGroup - The group to which the metric belongs. This is used to organize the metrics in the UI
    /// <param> metricsVariable - The variable to be tracked as a metric
    /// <param> metricsDescription - A description of the metric
+   /// <param> graph - Is this variable meant to be visually graphed in the profiler
+   /// <param> graphColor - If graphed, what color is used
+   /// <param> graphIntervalRate - If graphed, what is the polling rate on the graph?
    /// </summary>
-   void addMetric(StringTableEntry metricsGroup, const String& metricsVariable, const String& metricsDescription);
+   void addMetric(StringTableEntry metricsGroup, const String& metricsVariable, const String& metricsDescription, const bool& graph = false,
+      const LinearColorF& graphColor = LinearColorF::WHITE, const S32& graphIntervalRate = 0);
 
    /// <summary>
    /// Removes a metric from the tracker
@@ -63,6 +71,13 @@ public:
    /// <param> index - The index of the metric
    /// </summary>
    const char* getMetricsDescription(StringTableEntry metricsGroup, const S32& index);
+
+   /// <summary>
+   /// Returns the graph info of the metric at the specified index
+   /// <param> metricsGroup - The group to which the metrics belong
+   /// <param> index - The index of the metric
+   /// </summary>
+   const char* getMetricsGraphInfo(StringTableEntry metricsGroup, const S32& index);
 };
 
 extern MetricsTracker gMetricsTracker;
